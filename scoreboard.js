1ef458f633e0f16c1ede3f4cc4d18fa5f231df51
Mike Shenk <mlshenk415@gmail.com>
2020-06-17 18:16:40 -0700

    major fixes to js

---
+++ scoreboard.js
@@ -0,0 +1,18 @@
+function showFinalScore(){
+    // either get scores from localstorage or set to empty array
+  var highscores = JSON.parse(window.localStorage.getItem("highScoreList")) || [];
+  highscores.forEach(function(score) {
+      // create li tag for each high score
+      var liTag = document.createElement("li");
+      liTag.textContent = score.initials + " - " + score.score;
+      // display on page
+      var olEl = document.getElementById("scoreList");
+      olEl.appendChild(liTag);
+    });
+}
+showFinalScore();
+
+function clearHighScores() {
+    window.localStorage.removeItem("highScoreList");
+    window.location.reload();
+  }
--- script.js
+++ script.js
@@ -15,149 +15,50 @@
 // //create function for timer, run function when user starts quiz
 // //if user answers incorrectly, deduct time. if/else? -->
 
-// console("Hi");
-
-// function runQuiz() {
-
-// var questionNum = (12345678) {
-// let question = [];
-// [0] = "Question 1";
-// [1] = "Question 2";
-// [2] = "Question 3";
-// [3] = "Question 4";
-// [4] = "Question 5";
-// [5] = "Question 6";
-// [6] = "Question 7";
-// [7] = "Question 8";
-// [8] = "Question 9";
-// }
-
-// var score = "0";
-
-// for[0] {
-//     var correctAns = "";
-//     correctAns === true; 
-//     var wrongAns = "___";
-//     wrongAns !== true;
-//     var wrongAns2 = "___";
-//     wrongAns2 !== true;
-//     var wrongAns3 = "___";
-//     wrongAns !== true;
-// }
-// for[1] {
-//     var correctAns = "";
-//     correctAns === true; 
-//     var wrongAns = "___";
-//     wrongAns !== true;
-//     var wrongAns2 = "___";
-//     wrongAns2 !== true;
-//     var wrongAns3 = "___";
-//     wrongAns !== true;
-// }
-// for[2] {
-//     var correctAns = "";
-//     correctAns === true; 
-//     var wrongAns = "___";
-//     wrongAns !== true;
-//     var wrongAns2 = "___";
-//     wrongAns2 !== true;
-//     var wrongAns3 = "___";
-//     wrongAns !== true;
-// }
-// for[3] {
-//     var correctAns = "";
-//     correctAns === true; 
-//     var wrongAns = "___";
-//     wrongAns !== true;
-//     var wrongAns2 = "___";
-//     wrongAns2 !== true;
-//     var wrongAns3 = "___";
-//     wrongAns !== true;
-// }
-// for[4] {
-//     var correctAns = "";
-//     correctAns === true; 
-//     var wrongAns = "___";
-//     wrongAns !== true;
-//     var wrongAns2 = "___";
-//     wrongAns2 !== true;
-//     var wrongAns3 = "___";
-//     wrongAns !== true;
-// }
-// for[5] {
-//     var correctAns = "";
-//     correctAns === true; 
-//     var wrongAns = "___";
-//     wrongAns !== true;
-//     var wrongAns2 = "___";
-//     wrongAns2 !== true;
-//     var wrongAns3 = "___";
-//     wrongAns !== true;
-// }
-// for[6] {
-//     var correctAns = "";
-//     correctAns === true; 
-//     var wrongAns = "___";
-//     wrongAns !== true;
-//     var wrongAns2 = "___";
-//     wrongAns2 !== true;
-//     var wrongAns3 = "___";
-//     wrongAns !== true;
-// }
-// for[7] {
-//     var correctAns = "";
-//     correctAns === true; 
-//     var wrongAns = "___";
-//     wrongAns !== true;
-//     var wrongAns2 = "___";
-//     wrongAns2 !== true;
-//     var wrongAns3 = "___";
-//     wrongAns !== true;
-// }
-// for[8] {
-//     var correctAns = "";
-//     correctAns === true; 
-//     var wrongAns = "___";
-//     wrongAns !== true;
-//     var wrongAns2 = "___";
-//     wrongAns2 !== true;
-//     var wrongAns3 = "___";
-//     wrongAns !== true;
-// }
 // +++++=======================================
 
 var questions = [{
 	id: "1",
     question: "1. How do you write a message in an alert box?",
     choices: ["msg('Hello World')", "msgBox('Hello World');", "alertBox('Hello World');", "alert('Hello World');"],
-    correctChoice: 2
+    correctChoice: "msgBox('Hello World');"
 }, {
 	id: "2",
     question: "2. How do you empty an array in JavaScript?",
     choices: ["arrayList[]", "arrayList(0)", "arrayList.length=0", "arrayList.len(0)"],
-    correctChoice: 2
+    correctChoice: "arrayList(0)"
 }, {
 	id: "3",
 	question: "3. What function adds an element at the begining of an array and one at the end?",
     choices: ["push,unshift", "unshift,push", "first,push", "unshift,last"],
-    correctChoice: 1
+    correctChoice: "push,unshift"
 }, {
 	id: "4",
     question: "4. What would following code return? console.log(typeof typeof 1);",
     choices: ["string", "number", "Syntax error", "undefined"],
-    correctChoice: 0
+    correctChoice: "number"
 },{
 	id: "5",
 	question: "5. Look at the following selector: $('div'). What does it select?",
     choices: ["The first div element", "The last div element", "All div elements", "Current div element"],
-    correctChoice: 2
+    correctChoice: "All div elements"
 }
 ]
+
+var totalScore = 0;
+var quizStarted = false;
+var cssStyling = 0;
+
+// document.getElementById("results").style.display = "none";
+
 // Question for Tutor: How do I get the text '# seconds left in the quiz" to appear next to the timer?
-var currentIndex = 0;
+var currentIndex= 0;
 function startQuiz(){
+    renderButtons();
+    quizStarted = true;
 var count = 90;
 displayQuestion();
+removeStartBtn();
 var interval = setInterval(function(){
   document.getElementById('count').innerHTML=count;
   count--;
@@ -167,7 +68,14 @@ var interval = setInterval(function(){
     // or...
 	alert("Time's up!!!");
   }
+
 }, 1000)}
+
+function removeStartBtn(){
+    var startNode = document.getElementById("startBtn");
+    startNode.remove();
+}
+
 
 // Making the questions appear
 function displayQuestion(){
@@ -176,21 +84,25 @@ function displayQuestion(){
 	};
 
 function incrementIndex(){
-	// console.log("value", currentIndex, "questionlength", questions.length);
+
+    console.log("value", currentIndex, "questionlength", questions.length);
     if(currentIndex===questions.length){
         endQuiz();
     } else {
+        clearFeedback();
+        currentIndex++;   
+        // document.getElementById("answer-confirmation").textContent = "";
         document.getElementById("question").innerHTML = questions[currentIndex].question;
         displayChoices(questions[currentIndex]);
-        currentIndex++;
     }
 	console.log("incremented", currentIndex);
 }
 function decrementIndex(){
-	// console.log("value", currentIndex, "questionlength", questions.length);
+    // document.getElementById("answer-confirmation").textContent = "";
+	console.log("value", currentIndex, "questionlength", questions.length);
     if(currentIndex === 0){
-        endQuiz();
     } else {
+        clearFeedback();
         currentIndex--;
         document.getElementById("question").innerHTML = questions[currentIndex].question;
         displayChoices(questions[currentIndex]);
@@ -216,32 +128,62 @@ function displayChoices(currentQuestion){
 function checkAnswer(){
     var displayElement = document.getElementById("answer-confirmation")
     if(this.value === questions[currentIndex].correctChoice) {
+        totalScore++;
         displayElement.textContent = "Correct!";
     } else {
         displayElement.textContent = "WRONG!";
     }
+    
     // console.log(this.value);
 }
 
+function clearFeedback(){
+    var clearFeedback = document.getElementById("answer-confirmation");
+    clearFeedback.textContent= "";
+}
 function endQuiz(){
     // display scorecard w/ Local Storage
-
-}
+    var playerInitials = document.getElementById("displayInitials").value.trim();
+    if (playerInitials !== ""){
+        var highScoreList = JSON.parse(window.localStorage.getItem("highScoreList")) || [];
+        var newScore = {
+            score: totalScore,
+            initials: playerInitials
+        };
+        //append values to the array 
+        highScoreList.push(newScore);
+        //set value in local storage under - highscorelist
+        window.localStorage.setItem("highScoreList",JSON.stringify(highScoreList))
+        window.location.href="scoreboard.html";
+    }
+} 
 // 	document.body.onload = addElement;
 
+
+function getResults(){
+    // document.getElementById("results)").style.display = "block";
+    document.getElementById("finalscore").textContent=totalScore;
+
+}
+
 function addElement(question) { 
-	document.getElementById("question").innerHTML = question.question;
+    document.getElementById("question").innerHTML = question.question;
+}
 
+function renderButtons() {
+    if(!quizStarted){
+    var nextBtnElement = document.getElementById("nextQuestion");
+    var nextBtn = document.createElement("button");
+        nextBtn.setAttribute("value", "next");
+        nextBtn.textContent = "next";
+        nextBtn.onclick = incrementIndex;
+        nextBtnElement.appendChild(nextBtn);
 
-
-// 	// create a new div element 
-// var newDiv = document.createElement("div"); 
-// 	  // and give it some content 
-// var newContent = document.createTextNode(question); 
-// 	  // add the text node to the newly created div
-// 	newDiv.appendChild(newContent);  
-	
-	  // add the newly created element and its content into the DOM 
-// var currentDiv = document.getElementById("div1"); 
-// 	document.body.insertBefore(newDiv, currentDiv); 
-	}
+    var prevBtnElement = document.getElementById("prevQuestion");
+    var prevBtn = document.createElement("button");
+        prevBtn.setAttribute("value", "prev");
+        prevBtn.textContent = "prev";
+        prevBtn.onclick = decrementIndex;
+        prevBtnElement.appendChild(prevBtn);
+    }
+};
